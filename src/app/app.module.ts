733f59a1d0953c42f47b7e6d96efd316f33eefe6
isabelalberti <isabelalberti84@gmail.com>
2022-02-21 09:36:59 -0300

    isabelalberti Iniciando Front - Easy Work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LoginComponent } from './login/login.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { FooterComponent } from './footer/footer.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginComponent,
+    JumbotronComponent,
+    FooterComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
