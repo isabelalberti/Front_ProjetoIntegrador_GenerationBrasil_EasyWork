b20a83c215046eb1cbd26568d2ec07e5cd139abc
Lucas <watchlucas274@gmail.com>
2022-02-25 11:25:09 -0300

    @Lucxsss[Adicionando enviroment/att service]

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { UserSecurityLogin } from '../model/UserSecurityLogin';
 import { Usuario } from '../model/Usuario';
 
@@ -25,4 +26,14 @@ export class AuthService {
       return this.http.post<Usuario>("https://projeto-integrador-grupo2.herokuapp.com/usuario/cadastrar", user)
 
 }
+
+logged(){
+  let ok: boolean = false
+
+if(environment.token != ''){
+ok = true
+}
+  return ok
+}
+
 }
