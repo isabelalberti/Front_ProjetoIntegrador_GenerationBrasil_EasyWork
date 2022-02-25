568529b3e6b13c8dc4a43bb362a106bfbcdf0b8b
Evelyn <evelynnunesrosa@gmail.com>
2022-02-25 12:55:20 -0300

    @evyrosa [Service de cadastro]

--- src/app/model/Usuario.ts
+++ src/app/model/Usuario.ts
@@ -1,20 +1,16 @@
-import { Card } from "./Card"
+import { Card } from './Card';
 
- export class Usuario{
-     
- public id: number
- public fullName: string
- public password: string
- public picture: string
- public autoBiography: string
- public email: string
- public phoneNumber: string
- public age: number
- public linkedin: string
- public gitHub: string
- public city: string
- public card: Card[]
-
-
-
+export class Usuario {
+    public id: number;
+    public fullName: string;
+    public password: string;
+    public picture: string;
+    public autoBiography: string;
+    public email: string;
+    public phoneNumber: string;
+    public age: number;
+    public linkedin: string;
+    public gitHub: string;
+    public city: string;
+    public card: Card[];
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+    user: Usuario
+    confPassword: string
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private auth: AuthService,
+        private router: Router
+    ) {}
 
+    ngOnInit() {
+        window.scroll(0, 0);
+    }
+
+    confirmPassword(event: any){
+        this.confPassword = event.target.value
+    }
+
+    register(event: any){
+        if (this.confPassword != this.user.password){
+            alert("As senhas não conferem!\nDigite novamente")
+        } else {
+            this.auth.register(this.user).subscribe((resp: Usuario) => {
+                this.user = resp
+                this.router.navigate(['/login'])
+                alert("Usuário cadastrado com sucesso!")
+            })
+        }
+    }
 }
